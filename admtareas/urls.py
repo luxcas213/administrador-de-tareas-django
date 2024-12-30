cd524ff1ba85deb3b6bdec6488c54fda4be014be
lucas <lgarbate@gmail.com>
2024-12-30 18:06:53 -0300

    crud con tasks

--- admtareas/urls.py
+++ admtareas/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('tasks/', views.tasks, name='tasks'),
     path('create_task/', views.create_task, name='create_task'),
     path('tasks/<int:task_id>', views.taskdetail, name='create_task'),
+    path('tasks/complete/<int:task_id>', views.complete_task, name='complete_task'),
+    path('tasks/delete/<int:task_id>', views.delete_task, name='delete_task'),
+    path('tasks/update/<int:task_id>', views.update_task, name='update_task'),
 ]
--- admtareas/views.py
+++ admtareas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required 
 from .forms import TaskForm
 from .models import Task
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 @login_required
 def tasks(request):
@@ -22,10 +23,46 @@ def create_task(request):
             new_task.save()
             return redirect('tasks')
         except ValueError:
-            render(request, 'create_task.html',{"form":TaskForm(),"error":"Bad data passed in. Try again."})
+            return render(request, 'create_task.html',{"form":TaskForm(),"error":"Bad data passed in. Try again."})
             
 
 @login_required
 def taskdetail(request,task_id):
     task = Task.objects.get(pk=task_id)
     return render(request, 'taskdetail.html',{"task":task})
+
+@login_required
+def complete_task(request,task_id):
+    if request.method == 'POST':
+        task = Task.objects.get(pk=task_id)
+        if task.completed == False:
+            task.completed = True
+        else:
+            task.completed = False    
+        task.save()
+        return redirect('tasks')
+    else:
+        return redirect('tasks')
+    
+@login_required
+def delete_task(request,task_id):
+    if request.method == 'POST':
+        task = Task.objects.get(pk=task_id)
+        task.delete()
+        return redirect('tasks')
+    else:
+        return redirect('tasks')
+
+@login_required
+def update_task(request,task_id):
+    task = Task.objects.get(pk=task_id)
+    if request.method == 'GET':
+        form = TaskForm(instance=task)
+        return render(request, 'update_task.html',{"task":task,"form":form})
+    else:
+        try:
+            form = TaskForm(request.POST,instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'update_task.html',{"task":task,"form":form,"error":"Bad data passed in. Try again."})
